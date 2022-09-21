b8337471fa81ca620799fee909c9c937335cf949
Jimi Syrjä <95481040+JimiSyrja@users.noreply.github.com>
2022-09-21 12:40:19 +0000

    Balletjes, raket, snelheid, aantal

--- Player.js
+++ Player.js
@@ -2,13 +2,15 @@ class Player {
   constructor(){
     this.playerHeight = 20;
     this.playerWidth = 20;
-    this.xpos = 200;
-    this.ypos = 300;
+    this.xpos = width/4;
+    this.ypos =   370;
     this.speed = 5;
   }
 
   draw(){
+    fill(250, 250, 51)
     rect(this.xpos, this.ypos, this.playerWidth, this.playerHeight);
+    
     if(ship.ypos < 0){
      ship.ypos = 400
     }
--- script.js
+++ script.js
@@ -7,17 +7,18 @@ function setup() {
   ship = new Player();
 
 //laat de asteroids random aantallen spawnen
-  for(var i = 0; i < 40; i++){
+  for(var i = 0; i < 70; i++){
     let randomY = Math.floor(Math.random() * 300);
     let randomX = Math.floor(Math.random() * 700);
     let randomVx = Math.floor(Math.random() * 5) + 1;
-    let randomVy = Math.floor(Math.random() * 5) + 1;
-    balls.push(new Ball(randomX,randomY,10,10,randomVx,randomVy,"red"));
+    //let randomVy = Math.floor(Math.random() * 5) + 1;
+    balls.push(new Ball(randomX,randomY,10,10,randomVx,0,"red"));
   }
 }
 
 function display() {
     // Spawnt de speler
+    
     rect(this.xpos, this.ypos, this.playerHeight, this.playerWidth);
   }
 
